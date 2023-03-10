712c799ef553b9b5fde6dcb4f1c09c10e577aced
Sahil <leviack629@gmail.com>
2023-03-10 19:26:05 +0530

    added client side

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='App-header'>
+        <video controls muted src="http://localhost:4000/video" type='video/mp4'>
+
+        </video>
+      </header>
+    </div>
+  );
+}
+
+export default App;
